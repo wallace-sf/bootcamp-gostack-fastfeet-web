755dcf62d8d6a4e27a9c2d17cd315515990bd3b7
unknown <Wallace@WALLACE-PC.marestdt.com>
2020-03-09 22:09:08 -0300

    Initial commit

---
+++ src/routes/Route.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+export default function RouteWrapper({
+  component: Component,
+  isPrivate,
+  ...rest
+}) {
+  const signed = false;
+  console.tron.log(isPrivate);
+
+  if (!signed && isPrivate) {
+    return <Redirect to="/" />;
+  }
+
+  if (signed && !isPrivate) {
+    return <Redirect to="/deliveries" />;
+  }
+
+  return <Route component={Component} {...rest} />;
+}
+
+RouteWrapper.propTypes = {
+  component: PropTypes.oneOfType([PropTypes.element, PropTypes.func])
+    .isRequired,
+  isPrivate: PropTypes.bool,
+};
+
+RouteWrapper.defaultProps = {
+  isPrivate: false,
+};
---
+++ src/routes/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+
+import Route from './Route';
+
+import SignIn from '~/pages/SignIn';
+import Deliveries from '../pages/Deliveries';
+import Deliverymen from '../pages/Deliverymen';
+import Recipients from '../pages/Recipients';
+import Problems from '../pages/Problems';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/deliveries" component={Deliveries} isPrivate />
+      <Route path="/deliverymen" component={Deliverymen} isPrivate />
+      <Route path="/recipients" component={Recipients} isPrivate />
+      <Route path="/problems" component={Problems} isPrivate />
+    </Switch>
+  );
+}
